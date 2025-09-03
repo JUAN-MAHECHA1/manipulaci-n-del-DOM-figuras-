d8ba64c32f50bbd4f94429e1cb34357a00ad667a
Juan Mahecha <mahechajuanda29@gmail.com>
2025-09-03 15:55:01 -0500

    cree la figura circulo

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// capturamos la figura a manipular
+const figura =document.getElementById('figure');
+
+function circulo(){
+   figura.style.borderRadius = "50%"
+   figura.style.background = "purple"
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('btnCircle').addEventListener('click', circulo);
+})
