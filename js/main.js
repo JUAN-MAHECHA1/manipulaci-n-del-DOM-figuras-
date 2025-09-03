ae9e1d163278f9bef105ae7cee1ce0f557a61c87
Juan Mahecha <mahechajuanda29@gmail.com>
2025-09-03 16:12:29 -0500

    cree la figura gif

--- js/main.js
+++ js/main.js
@@ -5,7 +5,16 @@ function circulo(){
    figura.style.borderRadius = "50%"
    figura.style.background = "purple"
 }
+function estrella() {
+    figura.style.clipPath = "polygon(50% 0%, 61% 35%, 98% 35%, \
+                              68% 57%, 79% 91%, 50% 70%, \
+                              21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+    figura.style.borderRadius = "0"; // quitamos el círculo
+    figura.style.background = "gold";
+}
 
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('btnCircle').addEventListener('click', circulo);
+    document.getElementById('btnStar').addEventListener('click', estrella)
+    
 })
